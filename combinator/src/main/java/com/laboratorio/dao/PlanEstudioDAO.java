d047efdf3dcf97ee4a80b8210850e58e76edd187
j.godoy277@gmail.com <j.godoy277@gmail.com@ba79b48a-b461-de2b-99db-8b486498b371>
2014-04-05 20:12:02 +0000

    solo comente la clase porq todavia no la usamos, para que no forme parte del % de test no usados!xD

--- combinator/src/main/java/com/laboratorio/dao/PlanEstudioDAO.java
+++ combinator/src/main/java/com/laboratorio/dao/PlanEstudioDAO.java
@@ -1,59 +1,59 @@
-package com.laboratorio.dao;
-
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
-import com.laboratorio.modelo.PlanEstudio;
-
-public class PlanEstudioDAO 
-{
-	private static PlanEstudioDAO instancia = null;
-
-	@PersistenceContext(unitName = "PU")
-	private static EntityManager em = null;
-
-	public static PlanEstudioDAO getInstancia() throws Exception 
-	{
-		if (instancia == null)
-		{
-			instancia = new PlanEstudioDAO();
-		}
-		em = com.laboratorio.dao.EntityManagerUtil.getNewEM();
-		return instancia;
-	}
-	
-	
-	public void alta(PlanEstudio PE) throws Exception 
-	{
-		try 
-		{
-			em.getTransaction().begin();
-			em.merge(PE);
-			em.flush();
-			em.getTransaction().commit();
-		} 
-		catch (Exception e) 
-		{
-			em.getTransaction().rollback();
-			throw e;
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	public List<PlanEstudio> obtenerTodo() 
-	{
-		List<PlanEstudio> a = null;
-		Query query = em.createQuery("from PlanEstudios");
-		a = query.getResultList();
-		return a;
-	}
-
-	public PlanEstudio getPlanEstudios(int id)
-	{
-		PlanEstudio S = em.find(PlanEstudio.class, id);
-		return S;
-	}
-}
+//package com.laboratorio.dao;
+//
+//import java.util.List;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//import javax.persistence.Query;
+//
+//import com.laboratorio.modelo.PlanEstudio;
+//
+//public class PlanEstudioDAO 
+//{
+//	private static PlanEstudioDAO instancia = null;
+//
+//	@PersistenceContext(unitName = "PU")
+//	private static EntityManager em = null;
+//
+//	public static PlanEstudioDAO getInstancia() throws Exception 
+//	{
+//		if (instancia == null)
+//		{
+//			instancia = new PlanEstudioDAO();
+//		}
+//		em = com.laboratorio.dao.EntityManagerUtil.getNewEM();
+//		return instancia;
+//	}
+//	
+//	
+//	public void alta(PlanEstudio PE) throws Exception 
+//	{
+//		try 
+//		{
+//			em.getTransaction().begin();
+//			em.merge(PE);
+//			em.flush();
+//			em.getTransaction().commit();
+//		} 
+//		catch (Exception e) 
+//		{
+//			em.getTransaction().rollback();
+//			throw e;
+//		}
+//	}
+//
+//	@SuppressWarnings("unchecked")
+//	public List<PlanEstudio> obtenerTodo() 
+//	{
+//		List<PlanEstudio> a = null;
+//		Query query = em.createQuery("from PlanEstudios");
+//		a = query.getResultList();
+//		return a;
+//	}
+//
+//	public PlanEstudio getPlanEstudios(int id)
+//	{
+//		PlanEstudio S = em.find(PlanEstudio.class, id);
+//		return S;
+//	}
+//}
