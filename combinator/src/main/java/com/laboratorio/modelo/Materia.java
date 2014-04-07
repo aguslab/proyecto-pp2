3e1c69bc83ece18848b2784f52ccaeb47bea4d1a
j.godoy277@gmail.com <j.godoy277@gmail.com@ba79b48a-b461-de2b-99db-8b486498b371>
2014-04-07 00:56:11 +0000

    comentado @GeneratedValue(strategy = GenerationType.IDENTITY) para que el id quede el que seteo en java y no lo cree la bd

--- combinator/src/main/java/com/laboratorio/modelo/Materia.java
+++ combinator/src/main/java/com/laboratorio/modelo/Materia.java
@@ -1,18 +1,19 @@
 package com.laboratorio.modelo;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Materias")
-public class Materia 
+public class Materia implements Serializable
 {
+	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	@Column(nullable = false)
 	private String nombre;
@@ -41,5 +42,35 @@ public class Materia
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Materia other = (Materia) obj;
+		if (id != other.id)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	
+	
 
 }
