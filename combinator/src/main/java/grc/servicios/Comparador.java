20d248462b8934e582bb5cfbde82dee5fc8b0c52
dn.agus <dn.agus@ba79b48a-b461-de2b-99db-8b486498b371>
2014-05-29 19:46:01 +0000

    Nuevo comparador que implementa comparator

---
+++ combinator/src/main/java/grc/servicios/Comparador.java
@@ -0,0 +1,15 @@
+package grc.servicios;
+
+import java.util.Comparator;
+
+abstract class Comparador implements Comparator<Recomendacion>
+{
+
+	@Override
+	public int compare(Recomendacion o1, Recomendacion o2) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
