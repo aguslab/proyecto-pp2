1abb9aeb42b13dddc5066571f7d1bac12b7da7dc
dn.agus <dn.agus@ba79b48a-b461-de2b-99db-8b486498b371>
2014-06-03 01:47:20 +0000

    Creamos el CriterioOrdenDAO para persistir la tabla con lo criterios

---
+++ combinator/src/main/java/grc/dao/CriterioOrdenDAO.java
@@ -0,0 +1,69 @@
+package grc.dao;
+
+import grc.dominio.Carrera;
+import grc.dominio.Curso;
+import grc.dominio.Horario;
+import grc.servicios.CriterioOrden;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class CriterioOrdenDAO
+{
+	private static CriterioOrdenDAO instancia = null;
+	@PersistenceContext(unitName = "PU")
+	private static EntityManager em = null;
+	public static CriterioOrdenDAO getInstancia() throws Exception
+	{
+		if (instancia == null)
+		{
+			instancia = new CriterioOrdenDAO();
+		}
+		em = EntityManagerUtil.getNewEM();
+		return instancia;
+	}
+
+	public void alta(CriterioOrden CO) throws Exception
+	{
+		int id = obtenerTodo().size();
+		if(CO.getId()<=id)
+			CO.setId(id+1);
+		try
+		{
+			em.getTransaction().begin();
+			em.merge(CO);
+			em.flush();
+			em.getTransaction().commit();
+		} catch (Exception e)
+		{
+			em.getTransaction().rollback();
+			throw e;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<CriterioOrden> obtenerTodo()
+	{
+		List<CriterioOrden> a = null;
+		Query query = em.createQuery("from CriterioOrden");
+		a = query.getResultList();
+		return a;
+	}
+
+	public CriterioOrden getCriterioOrden(int id)
+	{
+		CriterioOrden S = em.find(CriterioOrden.class, id);
+		return S;
+	}
+}
