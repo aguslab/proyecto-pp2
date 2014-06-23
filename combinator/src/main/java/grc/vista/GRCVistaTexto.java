0e75adbbc2baf40f750a5d24eb9a7115f038fe9c
j.godoy277@gmail.com <j.godoy277@gmail.com@ba79b48a-b461-de2b-99db-8b486498b371>
2014-06-23 04:11:25 +0000

    i18N add!

--- combinator/src/main/java/grc/vista/GRCVistaTexto.java
+++ combinator/src/main/java/grc/vista/GRCVistaTexto.java
@@ -1,10 +1,8 @@
 package grc.vista;
 
 import grc.controlador.GRCControlador;
-import grc.dominio.Curso;
-import grc.dominio.Horario;
 import grc.modelo.GRCModelo;
-import grc.servicios.Recomendacion;
+import grc.servicios.IdiomaElegido;
 
 import java.util.List;
 import java.util.Observable;
@@ -17,6 +15,29 @@ public class GRCVistaTexto extends Thread implements Observer
 	private GRCModelo modelo;
 	private GRCVista vista;
 	private boolean salirModoTxt;
+	private static final String KEY_DATOS_ACTUALIZADOS = "MSJ_DATOS_ACTUALIZADOS";
+	private static final String KEY_SIN_RECOMENDACIONES = "MSJ_SIN_RECOMENDACIONES_CON_FILTROS_SELECC";
+	private static final String KEY_BIENVENIDA = "MSJ_BIENVENIDA";
+	private static final String KEY_RECOS_DISPONIBLES = "MSJ_RECOS_DISPONIBLES";
+	private static final String KEY_AYUDA = "MSJ_AYUDA";
+	private static final String KEY_INDICAR_TURNOS = "MSJ_INDICAR_TURNOS";
+	private static final String KEY_OPCION_FILTRO_MAÑANA = "MSJ_OPCION_FILTRO_MAÑANA";
+	private static final String KEY_OPCION_FILTRO_TARDE = "MSJ_OPCION_FILTRO_TARDE";
+	private static final String KEY_OPCION_FILTRO_NOCHE = "MSJ_OPCION_FILTRO_NOCHE";
+	private static final String KEY_OPCION_QUITAR_FILTROS = "MSJ_OPCION_QUITAR_FILTROS";
+	private static final String KEY_OPCION_MOSTRAR_LISTA_RECOS = "MSJ_OPCION_MOSTRAR_LISTA_RECOS";
+	private static final String KEY_OPCION_ACTUALIZAR = "MSJ_OPCION_ACTUALIZAR";
+	private static final String KEY_OPCION_GUI = "MSJ_OPCION_GUI";
+	private static final String KEY_OPCION_AYUDA = "MSJ_OPCION_AYUDA";
+	private static final String KEY_OPCION_SALIR = "MSJ_OPCION_SALIR";
+	private static final String KEY_SALUDO_MODO_TEXTO = "MSJ_SALUDO_MODO_TEXTO";
+	private static final String KEY_INGRESE_OPCION_CORRECTA = "MSJ_INGRESE_OPCION_CORRECTA";
+	private static final String KEY_NO_HAY_RECOS_DISPONIBLES = "MSJ_NO_HAY_RECOS_DISPONIBLES";
+	private static final String KEY_CAMBIAR_IDIOMA = "MSJ_CAMBIAR_IDIOMA";
+
+	private IdiomaElegido labels;
+	private boolean actualizeYo;
+	private String ultimoMsg;
 
 	public GRCVistaTexto(GRCControlador controlador, GRCModelo model, GRCVista vista)
 	{
@@ -24,6 +45,9 @@ public class GRCVistaTexto extends Thread implements Observer
 		this.modelo = model;
 		this.vista = vista;
 		this.salirModoTxt = false;
+		this.ultimoMsg = "";
+		this.actualizeYo = true;
+		this.labels = new IdiomaElegido();
 	}
 
 	@Override
@@ -32,29 +56,36 @@ public class GRCVistaTexto extends Thread implements Observer
 		if (o instanceof GRCModelo)
 		{
 			this.modelo = (GRCModelo) o;
-			System.out.println("se actualizaron los datos, presione 'R' para actualizar");
 		}
+		if (this.actualizeYo)
+		{
+			this.actualizeYo = false;
+			return;
+		}
+		mostrarMensaje(labels.getIdiomaMsjs().getString(KEY_DATOS_ACTUALIZADOS));
 	}
 
 	private void mensajeInicial()
 	{
 		List<String> listaRecomendaciones = this.modelo.getListaRecomendacionesSugeridas();
-		if(listaRecomendaciones.isEmpty()){
-			System.out.println("No hay recomendaciones con estos filtros");
+		if (listaRecomendaciones.isEmpty())
+		{
+			mostrarMensaje((labels.getIdiomaMsjs().getString(KEY_SIN_RECOMENDACIONES)));
 			return;
 		}
-			
-		System.out.println(listaRecomendaciones);
+
+		mostrarMensaje(listaRecomendaciones.get(0));
 		System.out.println();
-		System.out.println("Hay " + listaRecomendaciones.size()
-				+ " recomendaciones disponibles");
+		mostrarMensaje(labels.getIdiomaMsjs().getString(KEY_RECOS_DISPONIBLES) + " "
+				+ listaRecomendaciones.size());
 	}
 
 	public void menuPrincipal() throws Exception
 	{
 		limpiarPantalla();
-		System.out.println("Bienvenido al Generador de Recomendaciones de Cursada");
+		mostrarMensaje(labels.getIdiomaMsjs().getString(KEY_BIENVENIDA).toUpperCase());
 		System.out.println();
+		voyAActualizarModelo();
 		this.controller.filtrarTurnos();
 		System.out.println();
 		mensajeInicial();
@@ -64,50 +95,58 @@ public class GRCVistaTexto extends Thread implements Observer
 
 	private void mensajeAyuda()
 	{
-		System.out.println("presione h para obtener ayuda con los comandos");
+		mostrarMensaje(labels.getIdiomaMsjs().getString(KEY_AYUDA));
 
 	}
 
 	private void mostrarAyuda()
 	{
 		System.out.println();
-		System.out
-				.println("Indique qué turnos tiene disponibles. En caso de ser más de uno,\n separe las opciones con punto y coma.Ejemplo: si puede mañana y tarde, entonces ingrese \"1;2\":");
-		System.out.println("1 - Mañana");
-		System.out.println("2 - Tarde");
-		System.out.println("3 - Noche");
-		System.out.println("4 - Quitar todos los filtros");
-		System.out.println("T - Mostrar Lista de Recomendaciones");
-		System.out.println("R - Actualizar");
-		System.out.println("V - GUI");
-		System.out.println("H - Ayuda");
-		System.out.println("0 - Salir");
+		System.out.println(labels.getIdiomaMsjs().getString(KEY_INDICAR_TURNOS));
+		System.out.println("1 - " + labels.getIdiomaMsjs().getString(KEY_OPCION_FILTRO_MAÑANA));
+		System.out.println("2 - " + labels.getIdiomaMsjs().getString(KEY_OPCION_FILTRO_TARDE));
+		System.out.println("3 - " + labels.getIdiomaMsjs().getString(KEY_OPCION_FILTRO_NOCHE));
+		System.out.println("4 - " + labels.getIdiomaMsjs().getString(KEY_OPCION_QUITAR_FILTROS));
+		System.out.println("T - "
+				+ labels.getIdiomaMsjs().getString(KEY_OPCION_MOSTRAR_LISTA_RECOS));
+		System.out.println("R - " + labels.getIdiomaMsjs().getString(KEY_OPCION_ACTUALIZAR));
+		System.out.println("V - " + labels.getIdiomaMsjs().getString(KEY_OPCION_GUI));
+		System.out.println("H - " + labels.getIdiomaMsjs().getString(KEY_OPCION_AYUDA));
+		System.out.println("I - " + labels.getIdiomaMsjs().getString(KEY_CAMBIAR_IDIOMA));
+		System.out.println("0 - " + labels.getIdiomaMsjs().getString(KEY_OPCION_SALIR));
 	}
 
 	private void seleccionarOpcion()
 	{
 		String opcionUsuario = "x";
 		boolean cerrarApp = false;
-		while (!Opciones().contains(opcionUsuario) && !salirModoTxt)
+		while (!salirModoTxt)
 		{
 			opcionUsuario = getOpcionUsuario();
 			switch (opcionUsuario) {
 				case "1" :
+					voyAActualizarModelo();
 					controller.filtrarManiana(true);
 					actualizar();
 					break;
 				case "2" :
+					voyAActualizarModelo();
 					controller.filtrarTarde(true);
 					actualizar();
 					break;
 				case "3" :
+					voyAActualizarModelo();
 					controller.filtrarNoche(true);
 					actualizar();
 					break;
 				case "4" :
+					voyAActualizarModelo();
 					controller.cambiarEstadoFiltroManiana(false);
+					voyAActualizarModelo();
 					controller.cambiarEstadoFiltroTarde(false);
+					voyAActualizarModelo();
 					controller.cambiarEstadoFiltroNoche(false);
+					voyAActualizarModelo();
 					controller.filtrarTurnos();
 					actualizar();
 					break;
@@ -125,11 +164,9 @@ public class GRCVistaTexto extends Thread implements Observer
 					break;
 				case "v" :
 					ejecutarVista();
-//					salirModoTxt = true;
 					break;
 				case "V" :
 					ejecutarVista();
-//					salirModoTxt = true;
 					break;
 				case "h" :
 					mostrarAyuda();
@@ -137,23 +174,28 @@ public class GRCVistaTexto extends Thread implements Observer
 				case "H" :
 					mostrarAyuda();
 					break;
+				case "i" :
+					cambiarIdioma();
+					break;
+				case "I" :
+					cambiarIdioma();
+					break;
 				case "0" :
-					System.out
-							.println("Felicitaciones por usar el mejor y único recomendador de cursadas");
+					mostrarMensaje(labels.getIdiomaMsjs().getString(KEY_SALUDO_MODO_TEXTO));
 					cerrarApp = true;
 					break;
 				default :
-					System.out.println("Ingrese una opción correcta.");
+					mostrarMensaje(labels.getIdiomaMsjs().getString(KEY_INGRESE_OPCION_CORRECTA));
 					break;
 			}
 		}
-		
+
 		if (cerrarApp)
 		{
 			System.exit(0);
-		} else if(!salirModoTxt)
+		} else if (!salirModoTxt)
 		{
-//			mensajeInicial();
+			// mensajeInicial();
 			seleccionarOpcion();
 		}
 	}
@@ -168,11 +210,12 @@ public class GRCVistaTexto extends Thread implements Observer
 		int i = 1;
 		for (String r : this.modelo.getListaRecomendacionesSugeridas())
 		{
-			System.out.println(i +" - " + r);
+			System.out.println(i + " - " + r);
 			i++;
 		}
-		if(i == 1){
-			System.out.println("No hay recomendaciones Disponibles");
+		if (i == 1)
+		{
+			mostrarMensaje(labels.getIdiomaMsjs().getString(KEY_NO_HAY_RECOS_DISPONIBLES));
 		}
 	}
 
@@ -181,28 +224,10 @@ public class GRCVistaTexto extends Thread implements Observer
 		mensajeInicial();
 	}
 
-	private String Opciones()
-	{
-		return "1,2,3,4,r,R,h,H,0";
-	}
-
-	// public void volverAMenuPrincipal() throws Exception
-	// {
-	// System.out.println();
-	// String entradaTeclado;
-	// do
-	// {
-	// System.out.println("Para volver al menu principal presiones 1");
-	// entradaTeclado = getOpcionUsuario();
-	// } while (!entradaTeclado.equals("1"));
-	// {
-	// menuPrincipal();
-	// }
-	// }
-
 	private String getOpcionUsuario()
 	{
 		mensajeAyuda();
+		@SuppressWarnings("resource")
 		String entradaTeclado = new Scanner(System.in).nextLine();
 		return entradaTeclado;
 	}
@@ -240,26 +265,26 @@ public class GRCVistaTexto extends Thread implements Observer
 		System.out.println();
 	}
 
-//	private void mostrarTodasLasRecomendaciones(List<Recomendacion> recos)
-//	{
-//		System.out.println();
-//		int i = 1;
-//		for (Recomendacion r : recos)
-//		{
-//			System.out.println();
-//			System.out.println("Recomendacion :" + i);
-//			i++;
-//			for (Curso c : r.getRecomendacion())
-//			{
-//				System.out.println("Curso: " + c.getMateria().getNombre());
-//				for (Horario h : c.getHorario())
-//				{
-//					System.out.println(h.getDia() + ": de " + h.getHoraInicio() + " a "
-//							+ h.getHoraFin() + "hs");
-//				}
-//			}
-//		}
-//	}
+	// private void mostrarTodasLasRecomendaciones(List<Recomendacion> recos)
+	// {
+	// System.out.println();
+	// int i = 1;
+	// for (Recomendacion r : recos)
+	// {
+	// System.out.println();
+	// System.out.println("Recomendacion :" + i);
+	// i++;
+	// for (Curso c : r.getRecomendacion())
+	// {
+	// System.out.println("Curso: " + c.getMateria().getNombre());
+	// for (Horario h : c.getHorario())
+	// {
+	// System.out.println(h.getDia() + ": de " + h.getHoraInicio() + " a "
+	// + h.getHoraFin() + "hs");
+	// }
+	// }
+	// }
+	// }
 
 	@Override
 	public void run()
@@ -272,5 +297,23 @@ public class GRCVistaTexto extends Thread implements Observer
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private void voyAActualizarModelo()
+	{
+		this.actualizeYo = true;
+	}
+
+	public void mostrarMensaje(String msg)
+	{
+		if (this.ultimoMsg.equals(msg))
+			return;
+		this.ultimoMsg = msg;
+		System.out.println(msg);
+	}
+
+	public void cambiarIdioma()
+	{
+		this.labels.cambiarIdiomaMsjs();
 	}
 }
